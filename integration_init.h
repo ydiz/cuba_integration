7cf8c16089ce166529ee97a3b4879b3c4ab63f16
Yidi Zhao <zyd2117@gmail.com>
2019-05-04 13:49:14 -0400

    add kaon

--- integration_init.h
+++ integration_init.h
@@ -14,7 +14,9 @@ void init(int argc, char **argv, Int_para &para)
                     ("term1_eps", po::value<double>(&para.term1_eps)->default_value(1e-2), "epsilon for calculating principal part in term 1")
                     ("algorithm,a", po::value<std::string>(&para.algorithm)->default_value(""), "Cuhre or Divonne")
                     ("term,t", po::value<std::string>(&para.term)->default_value(""), "term1, term2 or term3")
-                    ("x_for_p1,x", po::value<int>(&para.x_for_p1)->default_value(0), "x for all_with_p1")
+                    ("x_for_p1,x", po::value<int>(&para.x_for_p1)->default_value(0), "x for with_p1")
+                    ("M_hadron", po::value<double>(&para.M_hadron)->default_value(0), "Intial state hadron mass")
+                    ("M_lepton", po::value<double>(&para.M_lepton)->default_value(0), "Final state lepton mass")
                     ;
 
   po::variables_map vm;
@@ -28,12 +30,15 @@ void init(int argc, char **argv, Int_para &para)
   }
 
 	std::cout << std::string(20, '*') << std::endl;
+	std::cout << "Initial state hadron mass: " << para.M_hadron << std::endl;
+	std::cout << "Final state lepton mass: " << para.M_lepton << std::endl;
+	std::cout << std::string(20, '*') << std::endl;
 	std::cout << "p_interval: " << para.p_interval << std::endl;
 	std::cout << "eps_rel: " << para.eps_rel << std::endl;
 	std::cout << "term1_eps: " << para.term1_eps << std::endl;
 	std::cout << "algorithm: " << para.algorithm << std::endl;
 	std::cout << "term: " << para.term << std::endl;
-	if(para.term=="all_with_p1") std::cout << "x_for_p1: " << para.x_for_p1 << std::endl;
+	if(para.term=="with_p1") std::cout << "x_for_p1: " << para.x_for_p1 << std::endl;
 	std::cout << std::string(20, '*') << std::endl;
 
 }
--- test.cc
+++ test.cc
@@ -1,5 +1,3 @@
-// #include <qlat/qlat.h>
-
 #include "integration.h"
 #include "integration_init.h"
 
@@ -8,7 +6,8 @@ int main(int argc, char **argv) {
 	Int_para para;
 	init(argc, argv, para);
 
-	if(para.term == "all_with_p1") run_p1(para);	
-	else run(para);
+	if(para.term == "with_p1") run_p1(para);	
+	else if(para.term == "with_p3")  run_p3(para);
+  else assert(0);
 	return 0;
 }
